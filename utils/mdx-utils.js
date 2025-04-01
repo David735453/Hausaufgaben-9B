cec95383cea84eb91d74ae6314a7796253d6c85f
David <david.hogea2010@icloud.com>
2025-04-01 19:30:46 +0200

    updates

--- utils/mdx-utils.js
+++ utils/mdx-utils.js
@@ -1,4 +1,4 @@
-import fs from 'fs';
+import fs from 'node:fs';
 import path from 'path';
 import matter from 'gray-matter';
 import { serialize } from 'next-mdx-remote/serialize';
@@ -37,58 +37,4 @@ export const getPosts = () => {
   posts = sortPostsByDate(posts);
 
   return posts;
-};
-
-export const getPostBySlug = async (slug) => {
-  const postFilePath = path.join(POSTS_PATH, `${slug}.mdx`);
-  const source = fs.readFileSync(postFilePath);
-
-  const { content, data } = matter(source);
-
-  const mdxSource = await serialize(content, {
-    // Optionally pass remark/rehype plugins
-    mdxOptions: {
-      remarkPlugins: [remarkGfm],
-      rehypePlugins: [rehypePrism],
-    },
-    scope: data,
-  });
-
-  return { mdxSource, data, postFilePath };
-};
-
-export const getNextPostBySlug = (slug) => {
-  const posts = getPosts();
-  const currentFileName = `${slug}.mdx`;
-  const currentPost = posts.find((post) => post.filePath === currentFileName);
-  const currentPostIndex = posts.indexOf(currentPost);
-
-  const post = posts[currentPostIndex - 1];
-  // no prev post found
-  if (!post) return null;
-
-  const nextPostSlug = post?.filePath.replace(/\.mdx?$/, '');
-
-  return {
-    title: post.data.title,
-    slug: nextPostSlug,
-  };
-};
-
-export const getPreviousPostBySlug = (slug) => {
-  const posts = getPosts();
-  const currentFileName = `${slug}.mdx`;
-  const currentPost = posts.find((post) => post.filePath === currentFileName);
-  const currentPostIndex = posts.indexOf(currentPost);
-
-  const post = posts[currentPostIndex + 1];
-  // no prev post found
-  if (!post) return null;
-
-  const previousPostSlug = post?.filePath.replace(/\.mdx?$/, '');
-
-  return {
-    title: post.data.title,
-    slug: previousPostSlug,
-  };
 };
